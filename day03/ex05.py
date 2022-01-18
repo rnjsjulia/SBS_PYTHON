eaa67e6ecff33c43e76fc192eadba522d1b7b29e
rnjsulia <withljs1@gmail.com>
2022-01-18 15:21:24 +0900

    3일차까지

---
+++ day03/ex05.py
@@ -0,0 +1,30 @@
+# 딕셔너리
+# '사전' - (단어) : (뜻)
+# 키(key) : 값(value)을 한 쌍으로 구성한 컬렉션
+# - 인덱스가 존재하지 않는다.
+# 기호 : {key : vlaue }
+#dic = { 키1 : 값1, 키2: 값2, ... }
+# - 값1 = dic[키1]
+
+d = {'a': 'apple', 'b': 'banana' }
+print('d : ', d)
+
+print( 'd[\'a\'] : ', d['a'])
+print( 'd[\'b\'] : ', d['b'])
+
+# 딕셔너리의 요소 추가
+d['c'] =  'melon'
+print('d : ', d)
+
+#다른 추가 방법
+# : setdefault() 함수를 활용
+d.setdefault('d', 'dog')
+print('d :', d)
+
+#딕셔너리 요소 수정
+d. update(d = 'cat')
+print('d:', d)
+
+#딕셔너리 요소 삭제
+d.pop('d')
+print('d :',d)
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("테스트")
+print('test')
+# \t:
+# \n:
+print('박상우\t,24\t,의정부시\n')
+print(""" hi 
+      """)

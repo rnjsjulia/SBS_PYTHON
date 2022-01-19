715bb38b8b3d0d67cb392e4c3ee9514138411df3
rnjsulia <withljs1@gmail.com>
2022-01-19 15:35:14 +0900

    5

--- day04/ex03.py
+++ day04/ex03.py
@@ -12,7 +12,10 @@ print('b : {1}, a : {0}' .format(a, b))
 
 print('b : {1}, a : {0},{0}' .format(a, b))
 
-print('{0}하세요, 저는 권수경{1}, 과목은 파이썬{1} 다음에 봐요{0}'.format('안녕', '입니다'))
+print('{0}하세요, 저는 권수경{1}, 과목은 파이썬{1} 다음에 봐요 {0}'.format('안녕', '입니다'))
 
 tell,tel2,tel3 = '02', '222', '3333'
 print('연락처 : {0}-{1}-{2}' .format(tell,tel2,tel3))
+
+academy = 'SBS'
+print('name : {name}' .format(name = academy))
---
+++ day04/ex06.py
@@ -0,0 +1,11 @@
+name = input('이름을 입력해주세요 : ')
+age = input('나이를 입력해주세요 : ')
+
+print('이름 :', name)
+print('나이 :', age)
+
+print('이름 : {}',.format(name))
+print('나이 : {}',.format(age))
+
+print(f'이름 : {name}')
+print(f'나이 : {age}')

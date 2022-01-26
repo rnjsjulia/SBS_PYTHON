601b1a74eb5d1c8a8320827d489fac31328fcbee
rnjsulia <withljs1@gmail.com>
2022-01-26 15:54:54 +0900

    1/26

---
+++ day09/secure.py
@@ -0,0 +1,15 @@
+
+'''
+    개인정보를 마스킹하는 함수를 정의
+    김*수, 김**,200425-2******
+'''
+
+
+def secure_name(name):
+    return name[0] + '**'
+
+def secure_no(no):
+    return no[0:8] + '******'
+
+def secure_phone(phone):
+    return phone.replace(phone[4:8],'****')
